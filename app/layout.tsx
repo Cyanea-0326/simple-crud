9645f83ba4615b8c274834dd5c82ed95a4725f3b
Cyanea <g3tsuzumi@gmail.com>
2023-11-17 00:02:44 +0900

    URL change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Simple web-application Next.js and Supabase',
+  description: 'Understanding the basics of web apps and implementing CRUD',
 }
 
 export default function RootLayout({
